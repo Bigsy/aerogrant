3d703280e18c8ac76884f88c58ce5ea31c6a5362
Bill Hedworth <bill.hedworth@gmail.com>
2023-03-28 16:11:24 +0100

    update version for release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject aerogrant "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.bigsy/aerogrant "0.1.0"
+  :description "mashup of aero and integrant with some nice addons"
+  :url "https://github.com/Bigsy/aerogrant"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
