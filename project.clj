1e1c5e42b7ae7957a5a3ab7bd44802d6d77144b6
Bill Hedworth <bill.hedworth@gmail.com>
2024-08-30 15:07:06 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.clojars.bigsy/aerogrant "0.1.7"
+(defproject org.clojars.bigsy/aerogrant "0.1.8"
   :description "mashup of aero and integrant with some nice addons"
   :url "https://github.com/Bigsy/aerogrant"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [aero "1.1.6"]
-                 [cheshire "5.11.0"]
-                 [com.cognitect.aws/api "0.8.686"]
-                 [com.cognitect.aws/sts "847.2.1387.0"]
-                 [com.cognitect.aws/endpoints "1.1.12.626"]
-                 [com.cognitect.aws/secretsmanager "847.2.1387.0"]
-                 [integrant "0.8.1"]]
+                 [cheshire "5.13.0"]
+                 [com.cognitect.aws/api "0.8.692"]
+                 [com.cognitect.aws/sts "857.2.1574.0"]
+                 [com.cognitect.aws/endpoints "1.1.12.718"]
+                 [com.cognitect.aws/secretsmanager "868.2.1599.0"]
+                 [integrant "0.10.0"]]
   :repl-options {:init-ns aerogrant.core})
